ba818460fc23fd83db10b042608237492e107516
Mohammadagil <agil.rofikul.007@gmail.com>
2023-12-18 10:40:03 +0700

    Menampilkan data notes history dari server

--- src/pages/Calculator.jsx
+++ src/pages/Calculator.jsx
@@ -8,6 +8,7 @@ import InputForm from "../components/Elements/InputForm";
 import ResultDisplay from "../components/Fragments/ResultDisplay";
 import axios from "axios";
 import { useAuth } from "../components/hooks/AuthContext";
+import NotesHistory from "../components/Fragments/NotesHistory";
 
 const ErrorMessage = ({ message }) => {
   return <div className="text-red-500 text-sm mt-2">{message}</div>;
@@ -88,15 +89,6 @@ const Calculator = () => {
     }
   }, [auth]);
 
-  const formatDate = (isoDate) => {
-    const date = new Date(isoDate);
-
-    // Pembentukan format tanggal yang diinginkan
-    const formattedDate = `${date.getDate()}-${date.getMonth() + 1}-${date.getFullYear()}`;
-
-    return formattedDate;
-  };
-
   return (
     <body className="min-h-screen">
       <h1 className="text-center text-sky-800 text-2xl font-bold">Calc BMI!</h1>
@@ -119,52 +111,16 @@ const Calculator = () => {
       </div>
       {auth ? (
         <>
-          <p className="text-center text-sky-800 text-2xl font-bold mt-10">Result History</p>
-          <div>
-            <div className="mx-[100px] my-7 font-bold text-lg text-gray-600">Notes History</div>
-            <div className="flex mb-7">
-              {userData.length > 0 ? (
-                <div className="flex flex-col mx-[100px] py-7 gap-y-2 rounded-md shadow overflow-auto w-[500px] h-[400px] items-center bg-zinc-300 bg-opacity-10">
-                  {userData
-                    .sort((a, b) => b.id_result - a.id_result)
-                    .map((result) => (
-                      <div key={result.id_result} className="w-[400px] h-[100px] p-4 bg-gray-100 rounded-md shadow-md mb-3">
-                        {/* Tampilkan data pengguna, sesuaikan dengan properti yang ada pada objek userData */}
-                        <p className="font-bold text-gray-800 text-center mb-2 bg-red-200 rounded-md">Date: {formatDate(result.date)}</p>
-                        <div className="flex flex-row justify-between">
-                          <div>
-                            <p className="text-gray-600 text-center">
-                              Age : <span className="font-bold">{result.age}</span>
-                            </p>
-                            <p className="text-gray-600 text-center">
-                              Height : <span className="font-bold">{result.height}</span>
-                            </p>
-                          </div>
-                          <div>
-                            <p className="text-gray-600 text-center">
-                              BMI : <span className="font-bold">{result.bmi}</span>
-                            </p>
-                            <p className="text-gray-600 text-center">
-                              Calories : <span className="font-bold">{result.calories}</span>
-                            </p>
-                          </div>
-                          <div>
-                            <p className="text-gray-600 text-center">
-                              Weight : <span className="font-bold">{result.weight}</span>
-                            </p>
-                            <p className="text-gray-600 text-center">
-                              Ideal Weight : <span className="font-bold">{result.ideal_weight}</span>
-                            </p>
-                          </div>
-                        </div>
-
-                        {/* ... (tampilkan properti lainnya) */}
-                      </div>
-                    ))}
-                </div>
-              ) : (
-                <div className="text-center text-sky-800 text-2xl font-bold">No results available</div>
-              )}
+          <div className="text-center text-sky-800 text-2xl font-bold mt-10">Result History</div>
+          <div className="flex flex-col md:flex-row justify-center md:space-x-1.6rem py-6 md:py-10 gap-1.6rem">
+            <div>
+              <div className="mx-[100px] mb-3 font-bold text-lg text-gray-600">Notes History</div>
+              <div className="relative"> <NotesHistory userData={userData} /></div>
+            </div>
+           
+            <div className="w-full md:w-[31.25rem]">
+              <div className="mx-4 md:mx-[1.25rem] mb-3 md:mb-5 font-bold text-lg text-gray-600">Calculation Track</div>
+              <div className="relative"></div>
             </div>
           </div>
         </>
